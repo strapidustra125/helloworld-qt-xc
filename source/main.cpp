b3862e7f464788ea76751d91921e3afc93f79678
Nikolay Novikov <n.novikov@transtelematica.ru>
2025-07-10 12:23:12 +0400

    1.0.1 - struct (global) : игрища со структурой.

--- source/main.cpp
+++ source/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl("qrc:/qml/main.qml"));  // Загружаем QML
+    engine.load(QUrl("qrc:/ui/main.qml"));  // Загружаем QML
 
     return app.exec();
 }
